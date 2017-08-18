209595ad4c4109441977a760eb50c58d56b95604
theartofnowt <michaelgordon.brown1@gmail.com>
2017-08-18 15:13:32 +0100

    step 6

--- js/note-controller.js
+++ js/note-controller.js
@@ -1,2 +1,16 @@
+(function(exports){
+   function NoteController(element, noteListModel, noteListView) {
+     this._element = element;
+     // this._noteListModel = noteListModel;
+     noteListModel.create("Favourite drink: seltzer");
+     this._noteListView = new noteListView(noteListModel);
+   }
 
-document.getElementById("app").innerHTML = "howdy"
+   NoteController.prototype = {
+     display: function() {
+       this._element.innerHTML = this._noteListView.toHtml();
+     }
+   };
+
+   exports.NoteController = NoteController;
+ })(this);
